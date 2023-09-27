e5c1a03de4b6007a06e165980d2b9b0867ac0514
mdzz2048 <2434206717@qq.com>
2023-09-28 00:06:27 +0800

    perf: 更换触发方式

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import {
-    Plugin,
-    App,
-    I18N
-} from "siyuan";
+import { Plugin } from "siyuan";
 import { 
     addEvent, 
     removeEvent, 
@@ -13,20 +9,15 @@ import {
 const STORAGE_NAME = "menu-config";
 
 export default class PluginSample extends Plugin {
-    constructor(app: App, name: string, i18n: I18N) {
-        super({
-            app: app,
-            name: name,
-            i18n: i18n
-        });
-        this.listener = this.listener.bind(this);
-    }
+
+    Listener = this.listener.bind(this);
 
     async onload() {
-        this.eventBus.on('ws-main', (event) => {
-            if (event.detail['cmd'] === 'backgroundtask') {
-                this.addListHoverListener();
-            }
+        this.eventBus.once('loaded-protyle', () => {
+            this.addListHoverListener();
+        })
+        this.eventBus.on('loaded-protyle-dynamic', () => {
+            this.addListHoverListener();
         })
         this.eventBus.on('click-editorcontent', () => {
             this.addListHoverListener();
@@ -38,6 +29,15 @@ export default class PluginSample extends Plugin {
     }
     
     onunload() {
+        this.eventBus.once('loaded-protyle', () => {
+            this.removeListHoverListener();
+        })
+        this.eventBus.on('loaded-protyle-dynamic', () => {
+            this.removeListHoverListener();
+        })
+        this.eventBus.on('click-editorcontent', () => {
+            this.removeListHoverListener();
+        })
         console.log(this.i18n.byePlugin);
     }
     
@@ -71,18 +71,45 @@ export default class PluginSample extends Plugin {
             // 给每个列表创建一个触发块
             let triggerBlock = createTriggerBlock(element);
             // 重新注册鼠标悬浮事件
-            removeEvent(triggerBlock, 'mouseenter', this.listener);
-            addEvent(triggerBlock, 'mouseenter', this.listener);
+            addEvent(triggerBlock, 'mouseenter', this.Listener);
         }
+    }
+
+    removeListHoverListener() {
+        const FOLD_LIST_SELECTOR = '.protyle-wysiwyg [data-node-id].li[fold="1"] > .p';
+        const foldLists = document.querySelectorAll(FOLD_LIST_SELECTOR);
+        const PREVIEW_LIST_SELECTOR = '.protyle-wysiwyg [data-node-id][previewList]';
+        const previewList = document.querySelectorAll(PREVIEW_LIST_SELECTOR);
+        
+        // 移除所有触发块
+        previewList.forEach((element) => {
+            const childElements = element.children;
+            // 清除标记属性
+            element.removeAttribute('previewList');
+            // 清除触发块
+            for (let index = 0; index < childElements.length; index++) {
+                let childElement = childElements[index];
+                if (childElement.hasAttribute('triggerBlock')) {
+                    childElement.remove();
+                }
+            }
+        })
+        // 移除所有监听器
+        foldLists.forEach((element) => {
+            const triggerBlock = createTriggerBlock(element);
+            removeEvent(triggerBlock, 'mouseenter', this.listener);
+        })
     }
 
     listener(event: Event) {
         let element = event.target as Element;
-        // 编辑器加载完后，展开折叠的列表块
-        this.eventBus.once('loaded-protyle', () => {
-            setTimeout(() => { 
-                openListInFloating(element) 
-            }, 100);
-        });
+        // 加载 protyle 窗口不触发展开列表
+        if (element.textContent != "siyuan-plugin-fold-list-preview") {
+            // 编辑器加载完后，展开折叠的列表块
+            this.eventBus.once('loaded-protyle', () => {
+                // todo: 有时候切换的太快了，触发不及时会报错，暂时没找到解决方案
+                openListInFloating(element);
+            });
+        }
     }
 }
